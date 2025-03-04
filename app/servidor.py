eaf63cafe910287de86518a46b0f05857b58aed4
adventurescumm <gab05mas@gmail.com>
2025-03-04 11:08:27 -0300

    Função de buscar imoveis por tipo e teste (não funcional) implementados

--- app/servidor.py
+++ app/servidor.py
@@ -69,6 +69,40 @@ def get_imoveis_por_id(id):
     return jsonify({"imoveis": imovel_dict}), 200
 
 
+@app.route('/imoveis/<string:tipo>', methods=['GET'])
+def get_imoveis_por_tipo(tipo):
+    # conectar colm a base
+    conn = connect_db()
+
+    if conn is None:
+        return jsonify({"error": "Erro ao conectar ao banco de dados"}), 500
+
+    # se chegou até, tenho uma conexão válida
+    cursor = conn.cursor()
+    sql = "SELECT * from imoveis WHERE tipo = %s"
+    cursor.execute(sql, (tipo,))
+    results = cursor.fetchall()
+
+    if not results:
+        return jsonify({"erro": "Nenhum imovel com esse tipo encontrado"}), 404
+
+   
+    imoveis = []
+    for imovel in results:
+        imovel_dict = {
+            "id": imovel[0],
+            "logradouro": imovel[1],
+            "tipo_logradouro": imovel[2],
+            "bairro": imovel[3],
+            "cidade": imovel[4],
+            "cep": imovel[5],
+            "tipo": imovel[6],
+            "valor": imovel[7],
+            "data_aquisicao": imovel[8]
+        }
+        imoveis.append(imovel_dict)
+    return jsonify({"imoveis": imoveis}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/tests/test_servidor.py
+++ app/tests/test_servidor.py
@@ -29,7 +29,7 @@ def test_get_imoveis(mock_connect_db, client):
     # Simulamos o retorno do banco de dados
     mock_cursor.fetchall.return_value = [
         (1, "José Eiras Pinheiro", "Rua", "Barra da Tijuca", "Rio de Janeiro", "21240004", "casa em condomínio", 150000.00, "2018-01-31"),
-        (2, "Sem Saída", "Rua", "Centro", "São Paulo", "04552999", "mansão", 1000000.00, "2022-05-30"),
+        (2, "Sem Saída", "Rua", "Centro", "São Paulo", "04552999", "mansao", 1000000.00, "2022-05-30"),
     ]
 
     # Substituímos a função `connect_db` para retornar nosso Mock em vez de uma conexão real
@@ -45,7 +45,7 @@ def test_get_imoveis(mock_connect_db, client):
     expected_response = {
         "imoveis": [
             {"id": 1, "logradouro": "José Eiras Pinheiro", "tipo_logradouro": "Rua", "bairro": "Barra da Tijuca", "cidade": "Rio de Janeiro", "cep": "21240004", "tipo": "casa em condomínio", "valor": 150000.00, "data_aquisicao": "2018-01-31"},
-            {"id": 2, "logradouro": "Sem Saída", "tipo_logradouro": "Rua", "bairro": "Centro", "cidade": "São Paulo", "cep": "04552999", "tipo": "mansão", "valor": 1000000.00, "data_aquisicao": "2022-05-30"},
+            {"id": 2, "logradouro": "Sem Saída", "tipo_logradouro": "Rua", "bairro": "Centro", "cidade": "São Paulo", "cep": "04552999", "tipo": "mansao", "valor": 1000000.00, "data_aquisicao": "2022-05-30"},
         ]
     }
     assert response.get_json() == expected_response
@@ -100,5 +100,40 @@ def test_get_imoveis_por_id(mock_connect_db, client):
     # Verificamos se os dados retornados estão corretos
     expected_response = {
         "imoveis": {"id": 1, "logradouro": "José Eiras Pinheiro", "tipo_logradouro": "Rua", "bairro": "Barra da Tijuca", "cidade": "Rio de Janeiro", "cep": "21240004", "tipo": "casa em condomínio", "valor": 150000.00, "data_aquisicao": "2018-01-31"}  
+    }
+    assert response.get_json() == expected_response
+
+@patch("servidor.connect_db")  # Substituímos a função que conecta ao banco por um Mock
+def test_get_imoveis_por_tipo(mock_connect_db, client):
+    """Testa a rota /imoveis sem acessar o banco de dados real."""
+
+    # Criamos um Mock para a conexão e o cursor
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    # Configuramos o Mock para retornar o cursor quando chamarmos conn.cursor()
+    mock_conn.cursor.return_value = mock_cursor
+
+    # Simulamos o retorno do banco de dados
+    mock_cursor.fetchall.return_value = [
+        (1, "José Eiras Pinheiro", "Rua", "Barra da Tijuca", "Rio de Janeiro", "21240004", "casa em condomínio", 150000.00, "2018-01-31"),
+        (2, "Sem Saída", "Rua", "Centro", "São Paulo", "04552999", "mansao", 1000000.00, "2022-05-30"),
+    ]
+    
+
+    # Substituímos a função `connect_db` para retornar nosso Mock em vez de uma conexão real
+    mock_connect_db.return_value = mock_conn
+
+    tipo = 'mansao'
+
+    # Fazemos a requisição para a API
+    response = client.get(f"/imoveis/{tipo}")
+
+    # Verificamos se o código de status da resposta é 200 (OK)
+    assert response.status_code == 200
+
+    # Verificamos se os dados retornados estão corretos
+    expected_response = {
+        "imoveis": {"id": 2, "logradouro": "Sem Saída", "tipo_logradouro": "Rua", "bairro": "Centro", "cidade": "São Paulo", "cep": "04552999", "tipo": "mansao", "valor": 1000000.00, "data_aquisicao": "2022-05-30"}  
     }
     assert response.get_json() == expected_response
