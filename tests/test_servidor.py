a8b90d1a91bf580bea8ef386767aa1721c29671e
esthefanny-farias <esthefannysf@al.insper.edu.br>
2025-03-04 00:09:45 -0300

    Refactor project structure: remove old config and servidor files, update imports, and add new config file

--- tests/test_servidor.py
+++ tests/test_servidor.py
@@ -1,6 +1,12 @@
 import pytest
 from unittest.mock import patch, MagicMock
-from app.servidor import app, connect_db
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app.utils import connect_db
+from config import config
+from app.servidor import app
+
 
 @pytest.fixture
 def client():
