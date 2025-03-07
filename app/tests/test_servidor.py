19d8197ec37440f0e71f55a7538cc9d08fd541ee
esthefanny-farias <esthefannysf@al.insper.edu.br>
2025-03-07 10:56:16 -0300

    implementado a função de teste add_imoveis

--- app/tests/test_servidor.py
+++ app/tests/test_servidor.py
@@ -163,44 +163,44 @@ def test_get_imoveis_por_tipo_vazio(mock_connect_db, client):
     assert response.status_code == 404
     assert response.get_json() == {"erro": "Nenhum imovel com esse tipo encontrado"}
 
-# @patch("servidor.connect_db")
-# def test_add_imoveis(mock_connect_db, client):
-#     """Testa a rota /imoveis sem acessar o banco de dados real."""
+@patch("servidor.connect_db")
+def test_add_imoveis(mock_connect_db, client):
+    """Testa a rota /imoveis sem acessar o banco de dados real."""
 
    
-#     mock_conn = MagicMock()
-#     mock_cursor = MagicMock()
-#     mock_conn.cursor.return_value = mock_cursor
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
 
    
-#     mock_cursor.fetchall.return_value = [
-#         (1, "José Eiras Pinheiro", "Rua", "Barra da Tijuca", "Rio de Janeiro", "21240004", "casa em condomínio", 150000.00, "2018-01-31"),
-#         (2, "Sem Saída", "Rua", "Centro", "São Paulo", "04552999", "mansao", 1000000.00, "2022-05-30"),
-#     ]
+    mock_cursor.fetchall.return_value = [
+        (1, "José Eiras Pinheiro", "Rua", "Barra da Tijuca", "Rio de Janeiro", "21240004", "casa em condomínio", 150000.00, "2018-01-31"),
+        (2, "Sem Saída", "Rua", "Centro", "São Paulo", "04552999", "mansao", 1000000.00, "2022-05-30"),
+    ]
     
 
    
-#     mock_connect_db.return_value = mock_conn
+    mock_connect_db.return_value = mock_conn
 
-#     imovel = {
-#         "id": 3,
-#         "logradouro": "Rua Elvira Ferraz",
-#         "tipo_logradouro": "Rua",
-#         "bairro": "Vila olimpia",
-#         "cidade": "São Paulo",
-#         "cep": "12345678",
-#         "tipo": "apartamento",
-#         "valor": 1000000.00,
-#         "data_aquisicao": "2020-01-01"
-#     }
+    imovel = {
+        "id": 3,
+        "logradouro": "Rua Elvira Ferraz",
+        "tipo_logradouro": "Rua",
+        "bairro": "Vila olimpia",
+        "cidade": "São Paulo",
+        "cep": "12345678",
+        "tipo": "apartamento",
+        "valor": 1000000.00,
+        "data_aquisicao": "2020-01-01"
+    }
 
   
-#     response = client.post("/imoveis", json=imovel)
+    response = client.post("/imoveis", json=imovel)
 
-#     assert response.status_code == 201
+    assert response.status_code == 201
 
-#     expected_response = {
-#         "imovel": imovel
-#     }
-#     assert response.get_json() == expected_response
+    expected_response = {
+        "imovel": imovel
+    }
+    assert response.get_json() == expected_response
 
